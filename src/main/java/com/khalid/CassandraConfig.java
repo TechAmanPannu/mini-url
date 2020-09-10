db24c9d6d0a4da4944e4f68274caf4f10f396d3e
amantechpannu <amandeep.pannu8233@gmail.com>
2020-09-10 17:44:07 +0530

    cassandra to spring connection is successfully completed

--- src/main/java/com/khalid/CassandraConfig.java
+++ src/main/java/com/khalid/CassandraConfig.java
@@ -1,15 +1,15 @@
 package com.khalid;
 
-import com.datastax.oss.driver.api.core.auth.AuthProvider;
-import com.datastax.oss.driver.internal.core.auth.PlainTextAuthProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
@@ -19,7 +19,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     protected String getKeyspaceName() {
-        return "tinyurl";
+        return "\"mini_url\"";
     }
 
 
@@ -30,12 +30,32 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     protected int getPort() {
-        return 9042;
+        return Integer.valueOf(env.getProperty("spring.data.cassandra.port"));
     }
 
     @Override
     public SchemaAction getSchemaAction() {
-        return SchemaAction.valueOf(env.getProperty(""));
+        return SchemaAction.valueOf(env.getProperty("spring.data.cassandra.scheme"));
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return "DC1-K8Demo";
+    }
+
+    @Override
+    protected String getSessionName() {
+        return "K8Demo";
+    }
+
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+
+        return Collections.singletonList(CreateKeyspaceSpecification.createKeyspace("\"mini_url\"")
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication());
     }
 
 
---
+++ src/main/java/com/khalid/StringUtils.java
@@ -0,0 +1,12 @@
+package com.khalid;
+
+public  class StringUtils {
+
+
+    public static void main(String arg[]){
+
+        String str = "\"mini_url\"";
+
+        System.out.println(str);
+    }
+}
