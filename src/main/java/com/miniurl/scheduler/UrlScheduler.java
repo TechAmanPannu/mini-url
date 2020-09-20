5d6f2ab821f3e67593dbc52155f3cc0759062d7a
amantechpannu <amandeep.pannu8233@gmail.com>
2020-09-21 02:47:53 +0530

    configure scheduler

---
+++ src/main/java/com/miniurl/scheduler/UrlScheduler.java
@@ -0,0 +1,19 @@
+package com.miniurl.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UrlScheduler {
+
+//    @Scheduled(cron = "0 0 10 15 * ?", zone = "Asia/Kolkata")
+    @Scheduled(fixedRate = 1000)
+    public void urlExpire() {
+
+        log.info("scheduler is running every second");
+
+
+    }
+}
